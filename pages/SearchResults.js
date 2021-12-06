c3f42c9752362d4b4a8375fd5174381fa0b9a111
solarjoni <solarjoni@gmail.com>
2021-12-06 22:46:25 +0200

    done

---
+++ pages/SearchResults.js
@@ -0,0 +1,6 @@
+class SearchResults {
+    get searchResultsLinks() {
+        return $$('.GyAeWb .LC20lb');
+    }
+}
+export default new SearchResults();
--- support/actions/search.js
+++ support/actions/search.js
@@ -1,15 +1,8 @@
+import homePage from "../../pages/Home";
 /**
  * Search for a keyword
  * @param {String} keyword keyword to search for
- * @param {object} inputField WebdriverIO input field element
- * @param {object} submitButton WebdriverIO submit button element
  */
-export default async (keyword, ggl_agree, /*  */inputField, submitButton) => {
-    await ggl_agree.waitForDisplayed(5000);
-    await ggl_agree.click();
-    await inputField.waitForDisplayed(5000);
-    await inputField.click();
-    await inputField.setValue(keyword);
-    await submitButton.waitForDisplayed(5000);
-    await submitButton.click();
+export default async (keyword) => {
+    await homePage.search(keyword);
 };
