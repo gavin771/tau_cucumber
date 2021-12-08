b0371ca2b3771ff59078822ed39f6f51fd81d7d3
solarjoni <solarjoni@gmail.com>
2021-12-08 18:21:00 +0200

    final

--- automationpractice/support/assertions/checkProductsContain.js
+++ automationpractice/support/assertions/checkProductsContain.js
@@ -1,4 +1,5 @@
-import catalog from "../../pom/Catalog"
+import catalog from "../../pom/Catalog";
+import assert from "assert";
 
 /**
  * @param {boolean} keyword search term to be present
@@ -6,6 +7,18 @@ import catalog from "../../pom/Catalog"
 
 export default async (keyword) => {
     const products = await catalog.products;
-    console.log("products: " + await products.length);
+    console.log("### products length: " + await products.length + " ###");
 
-}
+    for(const product of products) {
+        const productText = await product.getText()
+        var str = await productText.trim().toLowerCase();
+
+        if (str) {
+            console.log("productText: " + await str);
+            assert(
+                await str.includes(keyword),
+                `Product ${await product.getText()} does not contain ${keyword}`
+            );
+        }
+    };
+};
